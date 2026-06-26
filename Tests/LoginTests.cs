abf44ba40a44c82f001d6ecec70ee7884f7f62ec
YevheniiaVakulenko <zhenia2607.cool@gmail.com>
2026-06-26 15:25:16 +0100

    Add POM structure

--- Tests/LoginTests.cs
+++ Tests/LoginTests.cs
@@ -1,3 +1,4 @@
+using AutomationExerciseTests.Pages;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -5,11 +6,12 @@ using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using WebDriverManager.Helpers;
 
-namespace AutomationExerciseTests
+namespace AutomationExerciseTests.Tests
 {
     public class LoginTests
     {
         private IWebDriver driver;
+        private HomePage homePage;
 
         [SetUp]
         public void Setup()
@@ -17,18 +19,25 @@ namespace AutomationExerciseTests
             new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://automationexercise.com");
+            homePage = new HomePage(driver);
+            homePage.AcceptCookiesIfPresent();
         }
 
         [Test]
         public void HomePage_ShouldLoad_WithCorrectTitle()
         {
-            driver.Navigate().GoToUrl("https://automationexercise.com");
-
-            string title = driver.Title;
-
-            Assert.That(title, Is.EqualTo("Automation Exercise"));
+            Assert.That(homePage.GetTitle(), Is.EqualTo("Automation Exercise"));
         }
 
+        [Test]
+        public void Login_WithInvalidCredentials_ShouldShowError()
+        {
+            var loginPage = homePage.ClickSignupLogin();
+            loginPage.Login("nonexistent_user_99182@fake.com", "wrongpassword123");
+
+            Assert.That(loginPage.IsErrorMessageDisplayed(), Is.True);
+        }
         [TearDown]
         public void Teardown()
         {
